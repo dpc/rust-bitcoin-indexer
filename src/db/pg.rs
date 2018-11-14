2325e7b263306353c0c9f6cd37611991ebe2630e
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-11-13 17:43:02 -0800

    Use ugly, but fast multiple-value insertion

--- src/db/pg.rs
+++ src/db/pg.rs
@@ -2,7 +2,7 @@ use super::*;
 
 use dotenv::dotenv;
 use postgres::{transaction::Transaction, Connection, TlsMode};
-use std::{env, str::FromStr};
+use std::{env, fmt::Write, str::FromStr};
 
 pub fn establish_connection() -> Result<Connection> {
     dotenv().ok();
@@ -11,60 +11,154 @@ pub fn establish_connection() -> Result<Connection> {
     Ok(Connection::connect(database_url, TlsMode::None)?)
 }
 
-fn insert_parsed(transaction: &Transaction, parsed: &[Parsed]) -> Result<()> {
-    let insert_block = transaction
-        .prepare_cached("INSERT INTO blocks (height, hash, prev_hash) VALUES ($1, $2, $3)")?;
-    let insert_tx = transaction
-        .prepare_cached("INSERT INTO txs (height, hash, coinbase) VALUES ($1, $2, $3)")?;
-
-    let insert_input = transaction.prepare_cached(
-        "INSERT INTO inputs (height, utxo_tx_hash, utxo_tx_idx) VALUES ($1, $2, $3)",
-    )?;
-    let insert_output = transaction.prepare_cached(
-        "INSERT INTO outputs (height, tx_hash, tx_idx, value, address, coinbase) VALUES ($1, $2, $3, $4, $5, $6)",
-        )?;
-
-    for parsed in parsed {
-        let &Parsed {
-            ref block,
-            ref txs,
-            ref outputs,
-            ref inputs,
-        } = parsed;
-
-        insert_block.execute(&[
-            &(block.height as i64),
-            &block.hash.as_bytes().as_ref(),
-            &block.prev_hash.as_bytes().as_ref(),
-        ])?;
-
-        for tx in txs {
-            insert_tx.execute(&[
-                &(tx.height as i64),
-                &tx.hash.as_bytes().as_ref(),
-                &tx.coinbase,
-            ])?;
-        }
-
-        for input in inputs {
-            insert_input.execute(&[
-                &(input.height as i64),
-                &input.utxo_tx_hash.as_bytes().as_ref(),
-                &(input.utxo_tx_idx as i32),
-            ])?;
-        }
-
-        for output in outputs {
-            insert_output.execute(&[
-                &(output.height as i64),
-                &output.tx_hash.as_bytes().as_ref(),
-                &(output.tx_idx as i32),
-                &(output.value as i64),
-                &output.address,
-                &output.coinbase,
-            ])?;
-        }
+fn insert_blocks_query(blocks: &[Block]) -> Vec<String> {
+    if blocks.is_empty() {
+        return vec![];
     }
+    if blocks.len() > 9000 {
+        let mid = blocks.len() / 2;
+        let mut p1 = insert_blocks_query(&blocks[0..mid]);
+        let mut p2 = insert_blocks_query(&blocks[mid..blocks.len()]);
+        p1.append(&mut p2);
+        return p1;
+    }
+
+    let mut q: String = "INSERT INTO blocks (height, hash, prev_hash) VALUES".into();
+    for (i, block) in blocks.iter().enumerate() {
+        if i > 0 {
+            q.push_str(",")
+        }
+        q.write_fmt(format_args!(
+            "({}, '\\x{}', '\\x{}')",
+            block.height, block.hash, block.prev_hash,
+        ))
+        .unwrap();
+    }
+    q.write_str(";");
+    return vec![q];
+}
+fn insert_txs_query(txs: &[Tx]) -> Vec<String> {
+    if txs.is_empty() {
+        return vec![];
+    }
+    if txs.len() > 9000 {
+        let mid = txs.len() / 2;
+        let mut p1 = insert_txs_query(&txs[0..mid]);
+        let mut p2 = insert_txs_query(&txs[mid..txs.len()]);
+        p1.append(&mut p2);
+        return p1;
+    }
+
+    let mut q: String = "INSERT INTO txs (height, hash, coinbase) VALUES".into();
+    for (i, tx) in txs.iter().enumerate() {
+        if i > 0 {
+            q.push_str(",")
+        }
+        q.write_fmt(format_args!(
+            "({}, '\\x{}', {})",
+            tx.height, tx.hash, tx.coinbase,
+        ))
+        .unwrap();
+    }
+    q.write_str(";");
+    return vec![q];
+}
+
+fn insert_outputs_query(outputs: &[Output]) -> Vec<String> {
+    if outputs.is_empty() {
+        return vec![];
+    }
+    if outputs.len() > 9000 {
+        let mid = outputs.len() / 2;
+        let mut p1 = insert_outputs_query(&outputs[0..mid]);
+        let mut p2 = insert_outputs_query(&outputs[mid..outputs.len()]);
+        p1.append(&mut p2);
+        return p1;
+    }
+
+    let mut q: String =
+        "INSERT INTO outputs (height, tx_hash, tx_idx, value, address, coinbase) VALUES ".into();
+    for (i, output) in outputs.iter().enumerate() {
+        if i > 0 {
+            q.push_str(",")
+        }
+        q.write_fmt(format_args!(
+            "({}, '\\x{}', {}, {}, {}, {})",
+            output.height,
+            output.tx_hash,
+            output.tx_idx,
+            output.value,
+            output
+                .address
+                .as_ref()
+                .map_or("null".into(), |s| format!("'{}'", s)),
+            output.coinbase,
+        ))
+        .unwrap();
+    }
+    q.write_str(";");
+    return vec![q];
+}
+
+fn insert_inputs_query(inputs: &[Input]) -> Vec<String> {
+    if inputs.is_empty() {
+        return vec![];
+    }
+    if inputs.len() > 9000 {
+        let mid = inputs.len() / 2;
+        let mut p1 = insert_inputs_query(&inputs[0..mid]);
+        let mut p2 = insert_inputs_query(&inputs[mid..inputs.len()]);
+        p1.append(&mut p2);
+        return p1;
+    }
+
+    let mut q: String = "INSERT INTO inputs (height, utxo_tx_hash, utxo_tx_idx) VALUES ".into();
+    for (i, input) in inputs.iter().enumerate() {
+        if i > 0 {
+            q.push_str(",")
+        }
+        q.write_fmt(format_args!(
+            "({}, '\\x{}', {})",
+            input.height, input.utxo_tx_hash, input.utxo_tx_idx,
+        ))
+        .unwrap();
+    }
+    q.write_str(";");
+    return vec![q];
+}
+
+fn insert_parsed(conn: &Connection, parsed: Vec<Parsed>) -> Result<()> {
+    let mut bs = vec![];
+    let mut ts = vec![];
+    let mut is = vec![];
+    let mut os = vec![];
+
+    for parsed in parsed.into_iter() {
+        let Parsed {
+            mut block,
+            mut txs,
+            mut outputs,
+            mut inputs,
+        } = parsed;
+        bs.push(block);
+        ts.append(&mut txs);
+        is.append(&mut inputs);
+        os.append(&mut outputs);
+    }
+    for s in insert_txs_query(&ts) {
+        conn.batch_execute(&s)?;
+    }
+    for s in insert_inputs_query(&is) {
+        conn.batch_execute(&s)?;
+    }
+    for s in insert_outputs_query(&os) {
+        conn.batch_execute(&s)?;
+    }
+
+    for s in insert_blocks_query(&bs) {
+        conn.batch_execute(&s)?;
+    }
+
     Ok(())
 }
 pub struct Postresql {
@@ -121,9 +215,7 @@ impl Postresql {
                         let connection = establish_connection().unwrap();
 
                         while let Ok(parsed) = rx.recv() {
-                            let transaction = connection.transaction()?;
-                            insert_parsed(&transaction, &parsed);
-                            transaction.commit()?;
+                            insert_parsed(&connection, parsed);
                         }
                         Ok(())
                     }
