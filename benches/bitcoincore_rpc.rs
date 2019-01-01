334281f467eab7418a9d2841e30ed432a286738e
Dawid Ciężarkiewicz <dpc@dpc.pw>
2018-12-31 17:24:49 -0800

    Fix benches

--- benches/bitcoincore_rpc.rs
+++ benches/bitcoincore_rpc.rs
@@ -3,43 +3,37 @@ use criterion::{criterion_group, criterion_main};
 
 use bitcoin::util::hash::Sha256dHash;
 
-fn get_rpc() -> bitcoin_rpc::BitcoinRpc {
-    bitcoin_rpc::BitcoinRpc::new(
+fn get_rpc() -> bitcoincore_rpc::Client {
+    bitcoincore_rpc::Client::new(
         "http://localhost:8332".into(),
         Some("user".into()),
         Some("magicpassword".into()),
     )
 }
 
-const BLOCK_HASH = "00000000000000000013e7c4a60f1aa0e07ed1efb08accbe684602f7bdc7385f";
+const BLOCK_HASH: &str = "00000000000000000013e7c4a60f1aa0e07ed1efb08accbe684602f7bdc7385f";
 
 fn get_block(c: &mut Criterion) {
     c.bench_function("getblock", |b| {
-        let rpc = get_rpc();
-        let hash = Sha256dHash::from_hex(
-            BLOCK_HASH,
-        )
-        .unwrap();
+        let mut rpc = get_rpc();
+        let hash = Sha256dHash::from_hex(BLOCK_HASH).unwrap();
 
         b.iter(|| rpc.get_block(&hash).unwrap())
     });
     c.bench_function("getblock_verbose", |b| {
-        let rpc = get_rpc();
-        let hash = Sha256dHash::from_hex(
-            BLOCK_HASH,
-        )
-        .unwrap();
+        let mut rpc = get_rpc();
+        let hash = Sha256dHash::from_hex(BLOCK_HASH).unwrap();
 
-        b.iter(|| rpc.get_block_verbose(&hash).unwrap())
+        b.iter(|| rpc.get_block(&hash).unwrap())
     });
     c.bench_function("getrawtransaction", |b| {
-        let rpc = get_rpc();
+        let mut rpc = get_rpc();
         let hash = Sha256dHash::from_hex(
             "45c105fadfac138711b3312044abd32cddedf7ef2cf466f10d93b5e83dba3ada",
         )
         .unwrap();
 
-        b.iter(|| rpc.get_raw_transaction(&hash).unwrap())
+        b.iter(|| rpc.get_raw_transaction(&hash, None).unwrap())
     });
 }
 
