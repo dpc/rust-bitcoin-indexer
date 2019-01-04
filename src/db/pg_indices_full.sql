591a8d5d39ac3df298aa4573386098bec48c88d4
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-01-03 20:27:53 -0800

    Use `UNLOGGED` tables for speed

--- src/db/pg_indices_full.sql
+++ src/db/pg_indices_full.sql
@@ -4,3 +4,8 @@ CREATE INDEX IF NOT EXISTS outputs_height ON outputs (height);
 CREATE INDEX IF NOT EXISTS outputs_address_value ON outputs (address, value);
 CREATE INDEX IF NOT EXISTS outputs_tx_id_tx_idx ON outputs (tx_id, tx_idx);
 CREATE INDEX IF NOT EXISTS inputs_height ON inputs (height);
+
+ALTER TABLE blocks SET LOGGED;
+ALTER TABLE txs SET LOGGED;
+ALTER TABLE outputs SET LOGGED;
+ALTER TABLE inputs SET LOGGED;
