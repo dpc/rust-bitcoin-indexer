208007ef89558e17ce1c6b4e6873294c49a1daaa
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-01-29 00:19:10 -0800

    Fix `address_balances` for zero-balance addresses

--- src/db/pg_init_base.sql
+++ src/db/pg_init_base.sql
@@ -32,3 +32,15 @@ CREATE TABLE IF NOT EXISTS inputs (
   output_id BIGINT NOT NULL PRIMARY KEY,
   height BIGINT NOT NULL
 );
+
+DROP VIEW IF EXISTS address_balances;
+
+CREATE VIEW address_balances AS
+  SELECT address, SUM(
+    CASE WHEN inputs.output_id is NULL THEN value ELSE 0 END
+  ) AS value
+  FROM outputs
+      LEFT OUTER JOIN inputs ON outputs.id = inputs.output_id
+      INNER JOIN blocks on blocks.height = outputs.height
+  GROUP BY
+    outputs.address;
