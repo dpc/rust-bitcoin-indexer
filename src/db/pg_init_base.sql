e6eb712548deb47c9c060b664ea5eafb46704a93
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-01-29 20:23:12 -0800

    Balances by height

--- src/db/pg_init_base.sql
+++ src/db/pg_init_base.sql
@@ -35,12 +35,40 @@ CREATE TABLE IF NOT EXISTS inputs (
 
 DROP VIEW IF EXISTS address_balances;
 
+-- both outputs and inputs are `INNER JOIN`ed
+-- on `blocks`, as only after `block` is commited,
+-- we're sure that everything relevant to block
+-- was fully indexed;
+-- since it's so much complication, maybe we should
+-- just add everything in one big transaction
+-- to eliminate the need for it; hopefully this does not
+-- affect performance
 CREATE VIEW address_balances AS
   SELECT address, SUM(
-    CASE WHEN inputs.output_id is NULL THEN value ELSE 0 END
+    CASE WHEN inputs.output_id IS NULL THEN value ELSE 0 END
   ) AS value
   FROM outputs
-      LEFT OUTER JOIN inputs ON outputs.id = inputs.output_id
-      INNER JOIN blocks on blocks.height = outputs.height
+    INNER JOIN blocks AS blocks_outputs
+      ON blocks_outputs.height = outputs.height
+    LEFT JOIN inputs
+      INNER JOIN blocks AS blocks_inputs ON blocks_inputs.height = inputs.height
+    ON outputs.id = inputs.output_id
   GROUP BY
     outputs.address;
+
+DROP VIEW IF EXISTS address_balances_at_height;
+
+CREATE VIEW address_balances_at_height AS
+  SELECT address, blocks.height, SUM(
+    CASE WHEN outputs.height <= blocks.height AND inputs.output_id IS NULL THEN outputs.value ELSE 0 END
+  ) AS value
+  FROM blocks
+  LEFT JOIN outputs
+    ON true
+  LEFT JOIN inputs
+    ON outputs.id = inputs.output_id
+    AND inputs.height <= blocks.height
+  GROUP BY
+    blocks.height,
+    outputs.address
+  ORDER BY outputs.address;
