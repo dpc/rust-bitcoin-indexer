0483154297ff640fb4dd0999330a1443fa910465
Dawid Ciężarkiewicz <dpc@dpc.pw>
2019-01-31 23:27:56 -0800

    Upgrade to newer `rust-bitcoin`

--- benches/bitcoincore_rpc.rs
+++ benches/bitcoincore_rpc.rs
@@ -15,19 +15,19 @@ const BLOCK_HASH: &str = "00000000000000000013e7c4a60f1aa0e07ed1efb08accbe684602
 
 fn get_block(c: &mut Criterion) {
     c.bench_function("getblock", |b| {
-        let mut rpc = get_rpc();
+        let rpc = get_rpc();
         let hash = Sha256dHash::from_hex(BLOCK_HASH).unwrap();
 
         b.iter(|| rpc.get_block(&hash).unwrap())
     });
     c.bench_function("getblock_verbose", |b| {
-        let mut rpc = get_rpc();
+        let rpc = get_rpc();
         let hash = Sha256dHash::from_hex(BLOCK_HASH).unwrap();
 
         b.iter(|| rpc.get_block(&hash).unwrap())
     });
     c.bench_function("getrawtransaction", |b| {
-        let mut rpc = get_rpc();
+        let rpc = get_rpc();
         let hash = Sha256dHash::from_hex(
             "45c105fadfac138711b3312044abd32cddedf7ef2cf466f10d93b5e83dba3ada",
         )
--- src/prelude.rs
+++ src/prelude.rs
@@ -66,9 +66,7 @@ pub fn address_from_script(
         } else if script.is_p2pkh() {
             address::Payload::PubkeyHash(script.as_bytes()[3..23].into())
         } else if script.is_p2pk() {
-            let secp = secp256k1::Secp256k1::without_caps();
             let pubkey = match secp256k1::key::PublicKey::from_slice(
-                &secp,
                 &script.as_bytes()[1..(script.len() - 1)],
             ) {
                 Ok(p) => p,
